6fd8f0b56d9f1e27f3b139454e75847a82772567
Jon Rubio <jon.rubio.92@gmail.com>
2024-04-07 21:14:11 -0400

    Added script to build ISO

---
+++ build_iso/build_iso.sh
@@ -0,0 +1,11 @@
+# example image details:
+IMAGE_REPO=ghcr.io/osyki
+IMAGE_NAME=ekinoite
+IMAGE_TAG=latest
+IMAGE_VARIANT=Silverblue # should match the variant your image is based on
+# example installer details:
+INSTALLER_VERSION=39
+# iso command:
+sudo podman run --rm --privileged --volume .:/isogenerator/output --security-opt label=disable --pull=newer \
+-e IMAGE_REPO="$IMAGE_REPO" -e IMAGE_NAME="$IMAGE_NAME" -e IMAGE_TAG="$IMAGE_TAG" -e VARIANT="$IMAGE_VARIANT" \
+ghcr.io/ublue-os/isogenerator:$INSTALLER_VERSION
